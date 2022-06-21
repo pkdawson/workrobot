767242ef5b57b70d42bf7dfd9dee1d115d249185
Patrick Dawson <pat@dw.is>
2022-06-21 05:48:34 +0200

    Integrate bots with main loop

--- discord_bot.py
+++ discord_bot.py
@@ -3,29 +3,20 @@ from dotenv import load_dotenv
 load_dotenv()
 
 import disnake
-from disnake.ext import tasks
 
 class DiscordClient(disnake.Client):
-    def __init__(self, msg, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.msg = msg
         self.channel_id = int(os.getenv('DISCORD_CHANNEL'))
         self.role_id = int(os.getenv('DISCORD_ROLE'))
 
     async def on_ready(self):
-        print(f'We have logged in as {self.user}')
+        print(f'Discord client logged in as {self.user}')
+
+    async def send_message(self, msg):
+        await self.wait_until_ready()
         chan = self.get_channel(self.channel_id)
         if chan:
-            await chan.send(f'<@&{self.role_id}> {self.msg}')
-            # not sure how to cleanly terminate
-            await self.close()
+            await chan.send(f'<@&{self.role_id}> {msg}')
         else:
             raise Exception("channel not found")
-
-
-def send_message(msg):
-    client = DiscordClient(msg)
-    client.run(os.getenv('DISCORD_TOKEN'))
-
-if __name__ == '__main__':
-    send_message('hello world')
--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime, timezone, timedelta
 from common import *
 import time
@@ -9,11 +10,23 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from roll_seed import generate_seed
 from ruamel.yaml import YAML
 
+from discord_bot import DiscordClient
+from rtbot import WRBot
+import logging
+
 class Runner:
     def __init__(self):
         self.db = shelve.open('races.db')
         self.loop = asyncio.events.new_event_loop()
         self.scheduler = AsyncIOScheduler(event_loop=self.loop)
+        self.dc = DiscordClient()
+        self.logger = logging.getLogger()
+
+        self.rtbot = WRBot(
+            category_slug=os.getenv('RACETIME_CAT'),
+            client_id=os.getenv('RACETIME_CLIENT_ID'),
+            client_secret=os.getenv('RACETIME_CLIENT_SECRET'),
+            logger=self.logger)
 
     def load_schedule(self):
         yaml = YAML(typ='safe')
@@ -22,19 +35,37 @@ class Runner:
 
         for race in ydat['races']:
             tstr = race['datetime']
-            t = datetime.fromisoformat(tstr).replace(tzinfo=RACETZ)
             if tstr not in self.db:
                 print('new race')
-                self.db[tstr] = {'status': 'waiting'}
+                self.db[tstr] = {'status' : 'waiting'}
 
-            print(tstr, self.db[tstr])
+        now = datetime.now(timezone.utc)
+        for tstr in self.db:
+            t = datetime.fromisoformat(tstr).replace(tzinfo=RACETZ)
+            if t > now:
+                dt = t - now
+                if dt < timedelta(minutes=60):
+                    if self.db[tstr]['status'] == 'waiting':
+                        print('race soon')
+                        print(tstr)
+                        self.scheduler.add_job(self.prep_race, 'date', run_date=t, args=[tstr], id=f'{tstr}_prep')
+                        self.db[tstr]['status'] = 'scheduled'
 
-            # dt = t - datetime.now(timezone.utc)
-            # while dt > timedelta(minutes=35):
-            #     sleep_time = min(dt, timedelta(minutes=5))
-            #     print(f'Next race in {dt}, sleeping for {sleep_time}')
-            #     time.sleep(sleep_time.total_seconds())
-            #     dt = t - datetime.now(timezone.utc)
+    def prep_race(self, tstr):
+        pass
+
+    async def discord_loop(self):
+        await self.dc.start(os.getenv('DISCORD_TOKEN'))
+
+    async def dctest(self):
+        await self.dc.wait_until_ready()
+        await self.dc.send_message('hello send_message')
+
+    async def racetime_loop(self):
+        await self.rtbot.reauthorize()
+
+    async def racetime_refresh(self):
+        await self.rtbot.refresh_races()
 
     def run(self):
         asyncio.events.set_event_loop(self.loop)
@@ -42,6 +73,9 @@ class Runner:
 
         self.scheduler.add_job(self.load_schedule)
         self.scheduler.add_job(self.load_schedule, 'interval', minutes=5, id='load_schedule')
+        self.scheduler.add_job(self.discord_loop)
+        self.scheduler.add_job(self.racetime_loop)
+        self.scheduler.add_job(self.racetime_refresh)
 
         try:
             self.loop.run_forever()
